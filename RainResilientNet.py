bbf4f46c3bd012f2a127ab9f5db5808548027c60
dizizmi <65617120+dizizmi@users.noreply.github.com>
2025-04-26 12:06:00 +0800

    geemap visualisation - save to html to view

--- RainResilientNet.py
+++ RainResilientNet.py
@@ -3,6 +3,7 @@ import ee
 import os 
 os.environ['OMP_NUM_THREADS'] = '1'
 import geemap
+import webbrowser
 import pandas as pd
 import matplotlib.pyplot as plt 
 
@@ -22,9 +23,22 @@ def load_lst(start_date='2024-01-01', end_date='2025-04-20'):
 def main():
     ee.Initialize()
     lst_image = load_lst()
-
-    print('LST image loaded')
-
+    #Loading geemap 
+    Map = geemap.Map(center=[0, 0], zoom=2)
+    vis_params = {
+        'min': 0,
+        'max': 40,
+        'palette': ['blue', 'cyan', 'green', 'yellow', 'red']
+    }
+    #add lst image to map and display
+    Map.addLayer(lst_image, vis_params, 'LST (°C)')
+    Map.add_colorbar(vis_params, label="LST (°C)")
+    
+    #savemap to html
+    html_file = "lst_map.html"
+    Map.to_html(html_file)
+    print(f"Map has been saved to {html_file}.")
+    webbrowser.open(html_file)
 
 
 if __name__ == "__main__":
